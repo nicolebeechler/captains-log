736af444a9f212e90e22387a60c26109ce0552a1
Nicole Beechler <nmbeechler@gmail.com>
2024-01-02 22:24:45 -0500

    Added New Log form functionality.

--- server.js
+++ server.js
@@ -1,13 +1,56 @@
 const express = require('express')
+const jsxEngine = require('jsx-view-engine')
 const PORT = process.env.PORT || 3000
 
 const app = express()
 
+app.use(express.urlencoded({ extended: true })) 
+app.set('view engine', 'jsx')
+app.engine('jsx', jsxEngine())
+
 // Index Route
+
+// app.get('/log', async (req, res) => {
+//     try {
+//         const foundLogs = await Log.find({})
+//         res.render('log/Index', {
+//             log: foundLogs
+//         })
+//     } catch (error) {
+//         res.status(400).send({ message: error.message })
+//     }
+// })
 
 // New Route
 
+// app.get('/logs', (req, res) => {
+//     res.send('new')
+// })
+
+
+app.get('/logs', (req, res) => {
+    res.render('New')
+})
+
+// Delete Route
+
+// Update Route
+
 // Create Route
+
+// app.post('/logs', async (req, res) => {
+//     if (req.body.shipIsBroken === 'on') {
+//         req.body.shipIsBroken = true
+//     } else {
+//         req.body.shipIsBroken = false
+//     }
+//     try {
+//         const createdLog = await Log.create(req.body)
+//         res.redirect(`/logs/${createdLog._id}`)
+//     } catch(error) {
+//         res.status(400).send({message: error.message})
+//     }
+// })
 
 // Show Route
 
---
+++ views/New.jsx
@@ -0,0 +1,21 @@
+const React = require('react')
+
+function New (props) {
+    return (
+        <div>
+            <h1>New Log Page</h1>
+            <a href='/logs'>Go Back to Index Page</a>
+            <form action="/logs" method="POST">
+                Date: <input type="DateTime-local" name="date"/><br/>
+                Title: <input type="text" name="title"/><br/>
+                Entry: <input type="text" name="entry" /><br/>
+                Ship is Broken?: <input type="checkbox" name="shipIsBroken" /><br/>
+                <input type="submit" value="Create Log" />
+            </form>
+        </div>
+    )
+}
+
+// res.send('new')
+
+module.exports = New
