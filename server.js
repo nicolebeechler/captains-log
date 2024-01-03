9151368aa3d3851a41290cfdc6aaffcc3b6ee1a1
Nicole Beechler <nmbeechler@gmail.com>
2024-01-02 20:54:41 -0500

    Initial setup.

---
+++ server.js
@@ -0,0 +1,16 @@
+const express = require('express')
+const PORT = process.env.PORT || 3000
+
+const app = express()
+
+// Index Route
+
+// New Route
+
+// Create Route
+
+// Show Route
+
+app.listen(PORT, () => {
+  console.log(`${PORT} is working`)
+})
